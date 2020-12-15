025cb5f9e84bc88170569ae794463cbd18c7653d
giacomoratta <giacomo.ratta@diymaxeda.com>
2020-12-15 14:47:18 +0100

    19 - fix build issues with webpack versions

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -64,7 +64,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    // new webpack.HotModuleReplacementPlugin(),
     new HTMLWebpackPlugin({
       template: './src/index.ejs',
       inject: true,
